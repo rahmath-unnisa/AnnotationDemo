ba91a244dc152a920d494825afc3bea906f30174
Rahmath-Unnisa <asfiarehmath@gmail.com>
2022-05-21 00:12:13 +0530

    [Rahmath]Add:Executing Annotation Program

---
+++ AnnotationDemo/Employee.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnnotationDemo
+{
+    public class Employee
+    {
+        //<summary>
+        //<Gets or sets the name.
+        //<value>
+        //The name.
+        //</Value>
+        [Required(ErrorMessage = "Employee {0} is required")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Name should be minium 3 characters and of 100 characters")]
+        [DataType(DataType.Text)]
+        public string Name { get; set; }
+        //<summary>
+        //<Gets or sets the name.
+        //<value>
+        //The age.
+        //</Value>
+        [Range(18, 99, ErrorMessage = "Age should be above 18")]
+        public int Age { get; set; }
+        //<summary>
+        //<Gets or sets the name.
+        //<value>
+        //The phone number.
+        //</Value>
+        [DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; }
+        //<summary>
+        //<Gets or sets the name.
+        //<value>
+        //The email.
+        //</Value>
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+    }
+}
---
+++ AnnotationDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AnnotationDemo;
+class Program
+{
+    public static void Main(string[] args)
+    {
+       
+        Employee employee = new Employee();
+
+        employee.Name = "Mmqe";
+        employee.Age = 18;
+        employee.PhoneNumber = "5753012";
+        employee.Email = "asfiarehmath.com";
+        ValidationContext context = new ValidationContext(employee, null, null);
+        List<ValidationResult> results = new List<ValidationResult>();
+        bool valid=Validator.TryValidateObject(employee,context,results,true);
+        if (!valid)
+        {
+            foreach(ValidationResult Totalresult in results)
+            {
+                Console.WriteLine("Member Name: {0}",Totalresult.MemberNames.First(),Environment.NewLine);
+                Console.WriteLine("Error Msg : {0}{1}", Totalresult.ErrorMessage, Environment.NewLine);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Name: "+employee.Name+" "+"Age: "+employee.Age+" "+"PhoneNumber"+employee.PhoneNumber);
+        }
+        Console.WriteLine("Press any key to exit");
+
+    }
+}
